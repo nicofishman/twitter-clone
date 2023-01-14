5b357b427c4abcd1105fa5041d4c22e55d9cd4a3
nicofishman <nicourifishman@gmail.com>
2023-01-13 23:23:33 -0300

    configs

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,20 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    }
 };
+
 export default config;
