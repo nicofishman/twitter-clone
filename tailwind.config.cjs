fbde498b4a77bbf50a2863685bfeefaea17ba531
nicofishman <nicourifishman@gmail.com>
2023-01-14 14:05:27 -0300

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                lightGray: "#E7EAE9"
+            },
+            fontFamily: {
+                twitter: 'TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif'
+            }
+        },
     },
     plugins: [],
 };
