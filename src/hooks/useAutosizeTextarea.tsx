0f8293ed5b7990537c1eae96a739c5b6325e86d8
nicofishman <nicourifishman@gmail.com>
2023-01-27 20:59:22 -0300

    remove console.log

--- src/hooks/useAutosizeTextarea.tsx
+++ src/hooks/useAutosizeTextarea.tsx
@@ -15,10 +15,7 @@ const useAutosizeTextArea = (
 
             // We then set the height directly, outside of the render loop
             // Trying to set this with state or a ref will product an incorrect value.
-            textAreaRef.style.height = scrollHeight + "px";      
-            
-            console.log("scrollHeight: ", scrollHeight);
-            
+            textAreaRef.style.height = scrollHeight + "px";                  
         }
     }, [textAreaRef, thingsThatChange]);
 };
