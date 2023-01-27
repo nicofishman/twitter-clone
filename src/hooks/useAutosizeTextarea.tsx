4fe1c2199f0aefa668470896afcc12859d192769
nicofishman <nicourifishman@gmail.com>
2023-01-27 18:31:34 -0300

    AutoSizeTextarea hook

---
+++ src/hooks/useAutosizeTextarea.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+// Updates the height of a <textarea> when the value changes.
+const useAutosizeTextArea = (
+    textAreaRef: HTMLTextAreaElement | null,
+    ...thingsThatChange: any
+) => {
+    useEffect(() => {        
+        if (textAreaRef) {
+            // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+            textAreaRef.style.height = "0px";
+            textAreaRef.focus();
+            
+            const scrollHeight = textAreaRef.scrollHeight;
+
+            // We then set the height directly, outside of the render loop
+            // Trying to set this with state or a ref will product an incorrect value.
+            textAreaRef.style.height = scrollHeight + "px";      
+            
+            console.log("scrollHeight: ", scrollHeight);
+            
+        }
+    }, [textAreaRef, thingsThatChange]);
+};
+
+export default useAutosizeTextArea;
