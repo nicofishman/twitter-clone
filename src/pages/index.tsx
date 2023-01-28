69fb51b20f4b2f89f568b0542f00abffb4543e57
nicofishman <nicourifishman@gmail.com>
2023-01-27 21:00:35 -0300

    Add tuit component to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 import Layout from "../components/layout/Layout";
+import Avatar from "../components/common/Avatar";
+import { useUser } from "../utils/globalState";
+import Icon from "../components/common/Icon";
+import BottomSidebar from "../components/layout/BottomSidebar";
+import { api } from "../utils/api";
+import WriteTuitBox from '../components/index/WriteTuitBox';
+import Tuit from "../components/index/Tuit";
 
 const Home: NextPage = () => {
-    // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+    const user = useUser();
+    const { data: tuitsData } = api.tuit.getAll.useQuery();
 
     return (
         <>
@@ -14,9 +22,24 @@ const Home: NextPage = () => {
                 <link href="/favicon.ico" rel="icon" />
             </Head>
             <Layout>
+                <div className="[@media(min-width:500px)]:hidden flex items-center justify-center min-h-[56px]">
+                    <div className="absolute top-3 left-4">
+                        <Avatar alt={`${user.username}'s profile picture`} src={user.image} width={32} />
+                    </div>
+                    <Icon className="text-twitterBlue" name="twitter" />
+                </div>
                 <div className="px-4 flex items-center w-full h-14 border border-t-0 border-x-0 border-b-borderGray">
                     <p className="font-extrabold">Home</p>
                 </div>
+                <div className="[@media(min-width:500px)]:block hidden">
+                    <WriteTuitBox />
+                </div>
+                {
+                    tuitsData && tuitsData.map((tuit) => (
+                        <Tuit key={tuit.id} {...tuit}/>
+                    ))
+                }
+                <BottomSidebar />
             </Layout>
         </>
     );
