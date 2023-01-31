72bb5872ff23d13faf9702f04c8250aa2040ec8e
nicofishman <nicourifishman@gmail.com>
2023-01-31 11:59:17 -0300

    configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,13 +19,27 @@ module.exports = {
                 twitter: 'TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif'
             },
             animation: {
-                "spin-fast": "spin 0.7s linear infinite"
-            }
+                "spin-fast": "spin 0.7s linear infinite",
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
         },
         screens: {
             xxs: '500px',
             ...defaultTheme.screens
         }
     },
-    plugins: [],
+    plugins: [
+        require("tailwindcss-animate")
+    ],
 };
