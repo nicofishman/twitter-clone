092dd1e2699589f5c542b91a95bd5e7e631757fe
nicofishman <nicourifishman@gmail.com>
2023-02-02 15:13:46 -0300

    TuitInPage ReplyBox

---
+++ src/components/singleTuit/ReplyBox.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+
+import { useUser } from '@/utils/globalState';
+import useAutosizeTextArea from '@/hooks/useAutosizeTextarea';
+
+import Avatar from '../ui/Avatar';
+import DoTuitButton from '../Tuit/DoTuitButton';
+import Icon from '../ui/Icon';
+import { TuitButton } from '../index/Tuit';
+import TuitTextarea from '../Tuit/TuitTextarea';
+
+interface ReplyBoxProps {
+    tuitId: string;
+}
+
+const ReplyBox = ({}: ReplyBoxProps) => {
+    const user = useUser();
+    const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+    const [replyContent, setReplyContent] = useState('');
+    const [isTextareaFocused, setIsTextareaFocused] = useState(false);
+
+    useAutosizeTextArea(textAreaRef.current, replyContent);
+
+    const handleReply = () => {
+        if (!replyContent || replyContent === '') return;
+        console.log('Replying...');
+        setReplyContent('');
+    };
+
+    return (
+        <div className="flex min-h-[84px] w-full border-b border-borderGray px-4 pt-3">
+            <div className="h-full">
+                <Avatar user={user} width={48} />
+            </div>
+            <div className="ml-3 grow">
+                <div
+                    className={clsx('flex h-full', {
+                        'mb-3 flex-col': isTextareaFocused,
+                    })}
+                >
+                    <TuitTextarea
+                        className="min-h-[52px] pt-3"
+                        content={replyContent}
+                        doTuit={handleReply}
+                        placeholder="Tweet your reply"
+                        setContent={setReplyContent}
+                        onBlur={() => setIsTextareaFocused(false)}
+                        onFocus={() => setIsTextareaFocused(true)}
+                    />
+                    <div className="flex items-center justify-between">
+                        <div
+                            className={clsx('flex gap-x-2', {
+                                'mt-3 hidden w-full': !isTextareaFocused,
+                                'w-fit': isTextareaFocused,
+                            })}
+                        >
+                            <TuitButton tooltip="Media">
+                                <Icon
+                                    className="w-5 text-twitterBlue"
+                                    name="gallery"
+                                />
+                            </TuitButton>
+                            <TuitButton tooltip="GIF">
+                                <Icon
+                                    className="w-5 text-twitterBlue"
+                                    name="gif"
+                                />
+                            </TuitButton>
+                            <TuitButton tooltip="Emoji">
+                                <Icon
+                                    className="w-5 text-twitterBlue"
+                                    name="emoji"
+                                />
+                            </TuitButton>
+                            <TuitButton tooltip="Tag Location">
+                                <Icon
+                                    className="w-5 text-twitterBlue"
+                                    name="location"
+                                />
+                            </TuitButton>
+                        </div>
+                        <DoTuitButton
+                            className="py-[7px] px-4"
+                            doTuit={() => {}}
+                            tuitContent={replyContent}
+                        >
+                            Reply
+                        </DoTuitButton>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReplyBox;
--- src/components/singleTuit/TuitInPage.tsx
+++ src/components/singleTuit/TuitInPage.tsx
@@ -6,11 +6,12 @@ import Avatar from '../ui/Avatar';
 
 import TuitDateTime from './TuitDateTime';
 import DropdownThreeDots from './../Tuit/DropdownThreeDots';
+import ReplyBox from './ReplyBox';
 
 type TuitInPageProps = RouterOutputs['tuit']['getById'] & {};
 
 const TuitInPage = ({ author, ...tuit }: TuitInPageProps) => {
-    const wholeTuit: RouterOutputs['tuit']['getById'] = {
+    const wholeTuit = {
         author,
         ...tuit,
     };
@@ -18,40 +19,45 @@ const TuitInPage = ({ author, ...tuit }: TuitInPageProps) => {
     const user = useUser();
 
     return (
-        <article className="flex w-full flex-col px-4 pt-3">
-            <div className="border-b border-borderGray">
-                <header className="flex w-full justify-between">
-                    <div className="flex gap-x-3">
-                        <Avatar user={author} width={48} />
-                        <div className="flex-col">
-                            <h1 className="text-base font-bold">
-                                {author.full_name}
-                            </h1>
-                            <h2 className="text-base text-gray-500">
-                                @{author.username}
-                            </h2>
+        <>
+            <article className="flex w-full flex-col px-4 pt-3">
+                <div className="border-b border-borderGray">
+                    <header className="flex w-full justify-between">
+                        <div className="flex gap-x-3">
+                            <Avatar user={author} width={48} />
+                            <div className="flex-col">
+                                <h1 className="text-base font-bold">
+                                    {author.full_name}
+                                </h1>
+                                <h2 className="text-base text-gray-500">
+                                    @{author.username}
+                                </h2>
+                            </div>
                         </div>
+                        <DropdownThreeDots
+                            isSelfUser={tuit.authorId === user.id}
+                            tuitId={tuit.id}
+                            tuitUser={author}
+                        />
+                    </header>
+                    <div className="mt-3 w-full">
+                        <p className="text-2xl font-normal">{tuit.body}</p>
                     </div>
-                    <DropdownThreeDots
-                        isSelfUser={tuit.authorId === user.id}
-                        tuitId={tuit.id}
-                        tuitUser={author}
+                    <div className="my-4 w-full">
+                        <TuitDateTime date={tuit.createdAt} />
+                    </div>
+                </div>
+                <div className="border-b border-borderGray">
+                    <LikeCommentRetweet
+                        singleTuit
+                        className="max-w-[400px]"
+                        tuit={wholeTuit}
+                        userId={user.id}
                     />
-                </header>
-                <div className="mt-3 w-full">
-                    <p className="text-2xl font-normal">{tuit.body}</p>
                 </div>
-                <div className="my-4 w-full">
-                    <TuitDateTime date={tuit.createdAt} />
-                </div>
-                <LikeCommentRetweet
-                    singleTuit
-                    className="max-w-[400px]"
-                    tuit={wholeTuit}
-                    userId={user.id}
-                />
-            </div>
-        </article>
+            </article>
+            <ReplyBox tuitId={tuit.id} />
+        </>
     );
 };
 
